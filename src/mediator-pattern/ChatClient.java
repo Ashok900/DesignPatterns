07f78b7b07e0262b9b5bef6c33088f4b19710afa
Ashok900 <48505562+Ashok900@users.noreply.github.com>
2023-09-18 12:55:13 +0530

    Create ChatClient.java

---
+++ src/mediator-pattern/ChatClient.java
@@ -0,0 +1,18 @@
+package mediatorPattern;
+
+public class ChatClient {
+	public static void main(String[] args) {
+		ChatMediator mediator=new ChatMediatorImpl();
+		User user1=new UserImpl(mediator, "swan");
+		User user2=new UserImpl(mediator, "lisa");
+		User user3=new UserImpl(mediator, "yugan");
+		User user4=new UserImpl(mediator, "creta");
+		mediator.addUser(user1);
+		mediator.addUser(user2);
+		mediator.addUser(user3);
+		mediator.addUser(user4);
+		
+		user1.send("Hi All");
+	}
+
+}
