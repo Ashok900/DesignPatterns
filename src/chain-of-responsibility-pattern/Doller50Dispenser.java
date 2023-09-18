bce96993d175bcf1a2d165f0fca0208b5b9719ae
Ashok900 <48505562+Ashok900@users.noreply.github.com>
2023-09-18 13:20:26 +0530

    Create Doller50Dispenser.java

---
+++ src/chain-of-responsibility-pattern/Doller50Dispenser.java
@@ -0,0 +1,27 @@
+package chainofresponsibilityPattern;
+
+public class Doller50Dispenser implements DispenceChain{
+
+	private DispenceChain chain;
+	
+	@Override
+	public void setNextChain(DispenceChain nextChain) {
+		this.chain=nextChain;
+		
+	}
+
+	@Override
+	public void dispense(Currency cur) {
+		if(cur.getAmount()>=50) {
+			int num=cur.getAmount()/50;
+			int remainder=cur.getAmount()%50;
+			System.out.println("Dispensing "+num+" 50$ note");
+			if(remainder!=0) {
+				this.chain.dispense(new Currency(remainder));
+			}
+		}else {
+			this.chain.dispense(cur);
+		}
+	}
+
+}
