f7a90c7e62c2523ef0489f98a5795956760e63e9
Ashok900 <48505562+Ashok900@users.noreply.github.com>
2023-09-18 12:44:06 +0530

    Create ProxyPatternTest.java

---
+++ src/proxy-pattern/ProxyPatternTest.java
@@ -0,0 +1,21 @@
+package proxypattern;
+
+/*
+ * The definition itself is very clear and proxy design pattern is used when we want to provide controlled
+ * access of a functionality. Let’s say we have a class that can run some command on the system. Now if we 
+ * are using it, its fine but if we want to give this program to a client application, it can have severe 
+ * issues because client program can issue command to delete some system files or change some settings that
+ * you don’t want. Here a proxy class can be created to provide controlled access of the program
+ */
+public class ProxyPatternTest {
+	public static void main(String[] args) {
+		CommandExecutor executor=new CommandExecutorProxy("Ashok", "password");
+		try {
+			executor.runCommand("ls -ltr");
+			executor.runCommand(" rm -rf abc.pdf");
+		}catch(Exception e) {
+			System.out.println("Exception message::"+e.getMessage());
+		}
+	}
+
+}
