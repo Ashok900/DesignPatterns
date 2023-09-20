df09bd5e6f55205774a196e70450c8e50e1a166c
Ashok900 <48505562+Ashok900@users.noreply.github.com>
2023-09-20 10:06:35 +0530

    Create SportsCar.java

---
+++ src/decorator-pattern/SportsCar.java
@@ -0,0 +1,15 @@
+package decoratorPattern;
+
+public class SportsCar extends CarDecorator{
+
+	public SportsCar(Car car) {
+		super(car);
+	}
+	
+	@Override
+	public void assemble() {
+		super.assemble();
+		System.out.println("Adding features of assemble...");
+	}
+
+}
