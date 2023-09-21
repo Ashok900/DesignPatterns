287bc6a7d8371beab915d99b84cf1e14b219a1fe
Ashok900 <48505562+Ashok900@users.noreply.github.com>
2023-09-21 08:30:00 +0530

    Create OracleHelper.java

---
+++ src/facade-design-pattern/OracleHelper.java
@@ -0,0 +1,20 @@
+package facadepattern;
+
+import java.sql.Connection;
+
+public class OracleHelper {
+	
+	public static Connection getOracleConnection() {
+
+		return null;
+	}
+
+	public void generateOraclePDFReport(String tableName, Connection connection) {
+		//get data from table and generate pdf report
+	}
+
+	public void generateMySqlHTMLReport(String tableName, Connection connection) {
+		//get data from table and generate pdf report
+	}
+
+}
