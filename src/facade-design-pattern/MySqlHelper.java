e8aa63c00af042d8867f790bd3e6e07fec821286
Ashok900 <48505562+Ashok900@users.noreply.github.com>
2023-09-21 08:29:40 +0530

    Create MySqlHelper.java

---
+++ src/facade-design-pattern/MySqlHelper.java
@@ -0,0 +1,20 @@
+package facadepattern;
+
+import java.sql.Connection;
+
+public class MySqlHelper {
+
+	public static Connection getMySqlConnection() {
+
+		return null;
+	}
+
+	public void generateMySqlPDFReport(String tableName, Connection connection) {
+		//get data from table and generate pdf report
+	}
+
+	public void generateMySqlHTMLReport(String tableName, Connection connection) {
+		//get data from table and generate pdf report
+	}
+
+}
